38af58095d3487f1dedcec1e48a85a751b43b6bf
Maykon Valério da Silva <maykonvs@gmail.com>
2022-02-11 17:10:45 -0300

    Adicionando conceitos de type alias.

--- types.ts
+++ types.ts
@@ -62,3 +62,28 @@ message2 = "string nova";
 window.addEventListener("click", (e) => {
     console.log(e.target);
 })
+
+// Type alias
+type Uid = number | string | undefined
+
+function logDetails(uid: Uid, item: string) {
+    console.log(`A product with ${uid} has a title as ${item}.`);
+}
+
+function logInfo(uid: Uid, user: string) {
+    console.log(`An user with ${uid} has a name as ${user}.`);
+}
+
+type Platform = 'Windows' | 'Linux' | 'Mac OS' | 'ios'
+
+function renderPlatform(platform: Platform){
+    return platform
+}
+
+renderPlatform("ios");
+
+logDetails(123, 'sapato');
+logDetails("123", "sapato");
+
+logInfo(123, 'Willian');
+logInfo('123', 'Willian');
