e9b5069121e8f07b820e2037d7faeae9a335d54f
Maykon Valério da Silva <maykonvs@gmail.com>
2022-02-11 15:55:14 -0300

    Criando o tsconfig.json, configurando para colocar os arquivos compilados na pasta dist, removendo o .js antigo, e fazendo o html apontar para o arquivo dentro da pasta dist.

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const input1 = document.getElementById("num1") as HTMLInputElement;
 const input2 = document.getElementById("num2") as HTMLInputElement;
-const button = document.getElementById("button");
+const button = document.getElementById("button")!;  // ! indica que não é nullable.
 
 function sum(a: number, b: number) {
     return a + b;
